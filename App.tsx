3fe2fa300ae4f2daf0d134a437e433daa68df850
vigneshwaran <vigneshwaran.ravi@makoitlab.com>
2023-12-25 12:24:57 +0530

    migrated from expo to rn cli

---
+++ App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <Text>Hello world!!!!</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
--- app/index.jsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { Redirect } from "expo-router";
-
-const StartPage = () => {
-  return <Redirect href="/home" />;
-};
-
-export default StartPage;
